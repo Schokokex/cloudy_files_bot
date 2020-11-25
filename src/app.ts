75d38a1ca3b5068ee913dcb656edddfbc32b6566
JJ <github@jj22.de>
2020-11-25 12:37:25 +0000

    asd

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const port = 8080;
 if ('development' === app.get('env')) {
   app.get('/', (req, res) => {
     console.log(req);
-    res.send('<html><body><input id="in"></input></body><script>const $$ = document.getElementById; $$("in").addEventListener("keyup", ev => {if (ev.keyCode === 13) fetch("?msg="+$$("in").value)});</script></html>');
+    res.send("<html><body><input id='i'/></body><script>document.getElementById('i').addEventListener('keyup', ev => {if (ev.key === 'Enter') {fetch('?msg='+ev.target.value); ev.target.value=''}});</script></html>");
   })
 }
 // else process.env.NODE_ENV = 'production';
